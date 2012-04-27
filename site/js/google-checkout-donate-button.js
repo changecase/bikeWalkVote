bf86c6901f1806664c976ec8ac51ba6a77397566
Peter W <techieshark@gmail.com>
2012-04-26 18:46:09 -0700

    deleted old version of site

--- site/js/google-checkout-donate-button.js
+++
@@ -1,9 +0,0 @@
-function validateAmount(amount){
-	if(amount.value.match( /^[0-9]+(\.([0-9]+))?$/)){
-		return true;
-	}else{
-		alert('You must enter a valid contribution amount.');
-		amount.focus();
-		return false;
-	}
-}
